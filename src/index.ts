95bbdd575e139a40fd38060caa1cbdb488fc549f
Ran Yitzhaki <ranyitz@gmail.com>
2020-11-19 20:19:46 +0200

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,52 @@
 type Pattern = string;
 
-type Transform = ({ fileName, source }: { fileName: string, source: string }) => {}
+type Transform = ({
+  fileName,
+  source,
+}: {
+  fileName: string;
+  source: string;
+}) => {};
 
-type RegisterTask = (taskName: string, pattern: Pattern, transformFn: Transform) => void
+type RegisterTask = (
+  taskName: string,
+  pattern: Pattern,
+  transformFn: Transform
+) => void;
 
-type Migration = (registerTask: RegisterTask) => void
+type Migration = (registerTask: RegisterTask) => void;
 
 type RunMigration = () => void;
 
 type Options = { cwd: string };
 
-type CreateMigration = (options: Options, migration: Migration) => RunMigration
+type CreateMigration = (options: Options, migration: Migration) => RunMigration;
 
-type Task = { name: string, pattern: Pattern, transformFn: Transform };
+type Task = { name: string; pattern: Pattern; transformFn: Transform };
 
 import globby from 'globby';
 
 export const createMigration: CreateMigration = (options, migration) => {
-    const tasks: Array<Task> = [];
+  const tasks: Array<Task> = [];
 
-    const registerTask: RegisterTask = (name, pattern, transformFn) => {
-        tasks.push({ name, pattern, transformFn });
-    };
+  const registerTask: RegisterTask = (name, pattern, transformFn) => {
+    tasks.push({ name, pattern, transformFn });
+  };
 
-    migration(registerTask);
+  migration(registerTask);
 
-    console.log({ tasks, options });
-
-    return () => {
-        tasks.forEach(task => runTask(task, options));
-    };
+  return () => {
+    tasks.forEach((task) => runTask(task, options));
+  };
 };
 
 function runTask(task: Task, options: Options) {
-    const files = globby.sync(task.pattern, { cwd: options.cwd });
+  const files = globby.sync(task.pattern, { cwd: options.cwd });
 
-    if (!files) {
-        throw new Error('couldn');
-    }
+  console.log(files);
+
+  if (!files) {
+    //TODO handle file doesn't exist
+    throw new Error('couldn');
+  }
 }
