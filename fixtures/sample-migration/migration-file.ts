30aa0e0602e94675294fac23d93cc81150d6c962
Ran Yitzhaki <ranyitz@gmail.com>
2020-11-21 02:45:29 +0200

    fixtures

--- fixtures/sample-migration/migration-file.ts
+++ fixtures/sample-migration/migration-file.ts
@@ -1,4 +1,17 @@
-migrate('change bar to foo', ({ rename }) => {
+migrate('change bar to foo', ({ rename, transform, create, remove }) => {
+  create('baz.txt', () => {
+    return { fileName: 'src/baz.txt', source: 'baz' };
+  });
+
+  remove('remove foo.ts', 'src/foo.ts');
+
+  transform('change project name', 'package.json', ({ source }) => {
+    return {
+      fileName: 'package.json',
+      source: source.replace('sample-project', 'cool-project'),
+    };
+  });
+
   rename('bar.json to foo.json', '**/bar.json', ({ fileName }) => {
     return { fileName: fileName.replace('bar', 'foo') };
   });
