f6edb3da8658182ac111b6d65cade42c16d33e6f
Ran Yitzhaki <ranyitz@gmail.com>
2021-02-20 02:59:17 +0200

    remove redundant test

--- packages/code-migrate/__tests__/reportFile.test.ts
+++ packages/code-migrate/__tests__/reportFile.test.ts
@@ -2,45 +2,12 @@ import path from 'path';
 import { resolveFixture } from './utils';
 import { createTestkit } from 'code-migrate/testing';
 import fs from 'fs-extra';
-import execa from 'execa';
 
 const binFile = path.join(__dirname, '../bin/code-migrate');
 const fixtures = resolveFixture('full-with-errors');
 const migrationFile = path.join(fixtures, 'migration.ts');
 
-describe('default reporter', () => {
-  const testkit = createTestkit({
-    migrationFile,
-    migrationFunction: async ({ cwd }) => {
-      await execa(
-        'node',
-        [binFile, migrationFile, '--yes', '--reportFile=report.md'],
-        {
-          cwd,
-        }
-      );
-    },
-  });
-
-  test('reportFile', async () => {
-    const { cwd } = await testkit.run({
-      fixtures,
-    });
-
-    const reportPath = path.join(cwd, 'report.md');
-    const report = fs.readFileSync(reportPath, 'utf-8');
-
-    expect(report).toMatch('## automatic migration for my library');
-    expect(report).toMatch('#### add the build directory to .gitignore');
-    expect(report).toMatch('```\n.gitignore\n```');
-    expect(report).toMatch('**ERROR** `cool-config.json`');
-    expect(report).toMatch(
-      '```\nSyntaxError: Unexpected token f in JSON at position 1'
-    );
-  });
-});
-
-describe('quiet reporter', () => {
+describe('reportFile markdown report', async () => {
   const testkit = createTestkit({
     migrationFile,
     command: [
@@ -53,20 +20,18 @@ describe('quiet reporter', () => {
     ],
   });
 
-  test('reportFile', async () => {
-    const { cwd } = await testkit.run({
-      fixtures,
-    });
-
-    const reportPath = path.join(cwd, 'report.md');
-    const report = fs.readFileSync(reportPath, 'utf-8');
-
-    expect(report).toMatch('## automatic migration for my library');
-    expect(report).toMatch('#### add the build directory to .gitignore');
-    expect(report).toMatch('```\n.gitignore\n```');
-    expect(report).toMatch('**ERROR** `cool-config.json`');
-    expect(report).toMatch(
-      '```\nSyntaxError: Unexpected token f in JSON at position 1'
-    );
+  const { cwd } = await testkit.run({
+    fixtures,
   });
+
+  const reportPath = path.join(cwd, 'report.md');
+  const report = fs.readFileSync(reportPath, 'utf-8');
+
+  expect(report).toMatch('## automatic migration for my library');
+  expect(report).toMatch('#### add the build directory to .gitignore');
+  expect(report).toMatch('```\n.gitignore\n```');
+  expect(report).toMatch('**ERROR** `cool-config.json`');
+  expect(report).toMatch(
+    '```\nSyntaxError: Unexpected token f in JSON at position 1'
+  );
 });
